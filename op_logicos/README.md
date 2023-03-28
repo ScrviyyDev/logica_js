4effcfbbaa4104e5de3d900115810956722893db
scrviy <91909966+scrviy@users.noreply.github.com>
2023-03-28 16:09:13 -0300

    add readme

--- op_logicos/README.md
+++ op_logicos/README.md
@@ -1,5 +1,5 @@
 ## Operadores Lógicos em Javascript
 
 - && Operador AND === E => Todas as expressões precisam ser verdadeiras para retornar true.
-- || Operador OR === OU => 
+- || Operador OR === OU => Apenas uma expressão precisa ser verdadeira para retornar true.
 - ! Operador NOT === Não =>
--- op_logicos/index.js
+++ op_logicos/index.js
@@ -1,2 +1,13 @@
-console.log(true && true && true && true);
-console.log(true && true && false && true);
+const andExpression = true && true && true && true;
+console.log(andExpression);
+
+const orExpression = true || true || false || true;
+console.log(orExpression);
+
+const usuario = "Junior";
+const senha = "123456";
+
+const vaiLogar = usuario === "Junior" && senha === "12345";
+console.log(vaiLogar);
+
+console.log(!true);
